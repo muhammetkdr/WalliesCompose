060c22bd3cfdfedec747189072e29a8ae5587cab
oguzdogdu <oguzd494@gmail.com>
2024-11-22 15:54:22 +0300

    Fix version of ksp

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.kotlinAndroid) apply false
     alias(libs.plugins.compose.compiler) apply false
-    id("com.google.devtools.ksp") version "2.0.0-1.0.21" apply false
+    id("com.google.devtools.ksp") version "2.0.0-1.0.22" apply false
     id("com.google.dagger.hilt.android") version "2.49" apply false
     id("com.google.gms.google-services") version "4.4.0" apply false
     id("org.jetbrains.kotlin.plugin.serialization") version "1.9.24"
